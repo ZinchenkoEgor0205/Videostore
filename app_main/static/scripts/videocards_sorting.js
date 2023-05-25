24b40cee174ee2254d1fdd3511e9db8bd49bb6d5
Egor <zinchenko02051999@gmail.com>
2023-05-25 18:00:50 +0300

    updated the sorting page to dynamically sort goods when using price sort

--- app_main/static/scripts/videocards_sorting.js
+++ app_main/static/scripts/videocards_sorting.js
@@ -15,3 +15,34 @@ nameInput.addEventListener('input', () => {
     }, 1000)
     clearTimeout(timeout - 1)
 })
+
+let select = document.querySelector('#id_parameter')
+
+select.addEventListener('change', () => {
+    let selectValue = select.value
+    let videocardsPriceList = []
+    for (const videocard of videocards) {
+        let videocardPrice = Number(videocard.querySelector('.popular-content-positions-item-footer-link-price').textContent.split(' ')[0])
+        videocardsPriceList.push(videocardPrice)
+        videocard.remove()
+    }
+    if (selectValue === '1') {
+        videocardsPriceList.sort((a, b) => {
+            if (a < b) return -1
+            if (a === b) return 0
+            if (a > b) return 1
+        })
+    } else videocardsPriceList.sort((a, b) => {
+            if (a < b) return 1
+            if (a === b) return 0
+            if (a > b) return -1
+        })
+    for (const price of videocardsPriceList) {
+        for (const videocard of videocards) {
+            if (Number(videocard.querySelector('.popular-content-positions-item-footer-link-price').textContent.split(' ')[0]) === price) {
+                videocardsBlock.append(videocard)
+                continue
+            }
+        }
+    }
+})

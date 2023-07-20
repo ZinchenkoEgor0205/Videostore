3f8b21252b5cf3e1883be2ee1ab683dec4349e29
Egor <zinchenko02051999@gmail.com>
2023-07-20 09:33:12 +0300

    updated localization; fixed incorrect button's styles

--- app_user/forms.py
+++ app_user/forms.py
@@ -12,34 +12,34 @@ class AuthForm(forms.Form):
         self.fields['username'].localize = True
         self.fields['username'].widget.is_localized = True
 
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Имя пользователя'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Пароль'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Имя пользователя')}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Пароль')}))
 
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(
-        label=_("Username"),
+        label=_("Имя пользователя"),
         strip=False,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'placeholder': _('Имя пользователя')}),
         help_text=password_validation.password_validators_help_text_html(),
     )
     password1 = forms.CharField(
-        label=_("Password"),
+        label=_("Пароль"),
         strip=False,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'placeholder': _('Пароль')}),
         help_text=password_validation.password_validators_help_text_html(),
     )
     password2 = forms.CharField(
-        label=_("Password confirmation"),
+        label=_("Повторите пароль"),
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'placeholder': _('Повторите пароль')}),
         strip=False,
-        help_text=_("Enter the same password as before, for verification."),
+        help_text=_("Повторите ранее написанный пароль для верификации."),
     )
-    first_name = forms.CharField(max_length=30, required=False, help_text='Name', widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Имя'}))
-    last_name = forms.CharField(max_length=30, required=False, help_text='Last name', widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Фамилия'}))
-    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Email'}))
-    city = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Город'}))
-    phone = forms.CharField(required=False, widget=forms.NumberInput(attrs={'class': 'form-input input-gray', 'placeholder': 'Телефон'}))
+    first_name = forms.CharField(max_length=30, required=False, help_text='Name', widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Имя')}))
+    last_name = forms.CharField(max_length=30, required=False, help_text='Last name', widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Фамилия')}))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Email')}))
+    city = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Город')}))
+    phone = forms.CharField(required=False, widget=forms.NumberInput(attrs={'class': 'form-input input-gray', 'placeholder': _('Телефон')}))
 
     class Meta:
         model = User
@@ -48,22 +48,22 @@ class RegisterForm(UserCreationForm):
 
 
 class AccountEditForm(forms.Form):
-    username = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Имя пользователя'}))
+    username = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Имя пользователя')}))
     first_name = forms.CharField(required=False, max_length=30, help_text='Name',
-                                 widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Имя'}))
+                                 widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Имя')}))
     last_name = forms.CharField(required=False, max_length=30, help_text='Last name',
-                                widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Фамилия'}))
+                                widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Фамилия')}))
     email = forms.EmailField(required=False,
-                             widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'}))
+                             widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': _('Email')}))
     city = forms.CharField(required=False,
-                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Город'}))
+                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Город')}))
     phone = forms.CharField(required=False,
-                            widget=forms.NumberInput(attrs={'class': 'form-input', 'placeholder': 'Телефон'}))
+                            widget=forms.NumberInput(attrs={'class': 'form-input', 'placeholder': _('Телефон')}))
     street = forms.CharField(required=False,
-                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Улица'}))
+                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Улица')}))
     housing = forms.CharField(required=False,
-                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Корпус'}))
+                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Корпус')}))
     house = forms.CharField(required=False,
-                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Дом'}))
+                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Дом')}))
     apartment = forms.CharField(required=False,
-                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Квартира'}))
+                           widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': _('Квартира')}))
